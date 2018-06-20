3142afdfcd95715937780d711016ce191020ecc6
denevers <denevers1649@gmail.com>
2018-06-20 18:47:00 -0400

    first shot at creating OWL from GSML 3

---
+++ GeoSciMLv3.2/gsml3owl.sh
@@ -0,0 +1,5 @@
+ rm GeoSciMLv3.2_unversioned.EAP
+ unzip  GeoSciMLv3.2_unversioned.EAP
+ java -Djava.library.path=/c/sc/lib/ -jar /c/sc/ShapeChange-2.5.0.jar   -Dfile.encoding=UTF-8 -c sc/gsml3.xml
+ rm GeoSciMLv3.2_unversioned.EAP
+ 
